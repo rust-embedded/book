7eec4f7c018b954b6922500c27175ece8f858f04
Jorge Aparicio <jorge@japaric.io>
2018-09-26 16:51:48 +0200

    run linkchecker simulating the bookshelf layout
    this catches build errors in the bookshelf repo like
    https://travis-ci.org/rust-embedded/bookshelf/builds/433522599

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,15 @@ main() {
     mdbook build
 
     linkchecker book
+
+    # now check this as a directory of the bookshelf
+    rm -rf shelf
+    mkdir shelf
+    mv book shelf
+    linkchecker shelf
+
+    mv shelf/book .
+    rmdir shelf
 }
 
 main
